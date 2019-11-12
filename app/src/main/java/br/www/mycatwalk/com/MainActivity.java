87d725f853ebd5e67ffb2332b7dcff17705792e3
Gleidston <king2010-ro@hotmail.com>
2019-11-12 11:31:39 -0300

    Fase 1

--- app/src/main/java/br/www/mycatwalk/com/MainActivity.java
+++ app/src/main/java/br/www/mycatwalk/com/MainActivity.java
@@ -11,7 +11,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
-
+// fazer o codigo aqui
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
